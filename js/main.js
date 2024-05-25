ea6aa5f28783958dda71320adb150980237e4f7f
rudyhuacasi <alexisitaliano16@gmail.com>
2024-05-25 07:12:38 +0200

    {UPD} added javascript (message and picture)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 `use strict`
+
+// aggiungo il (data)
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        // il messaggio
+        message: `hello`,        
+        // immagine
+        width: `w-50`,
+        wolf: `https://wallpapersmug.com/download/3840x2160/14b73f/wolf-muzzle.jpg`,
+        // immagine centrare
+        center: `text-center`
+      }
+    }
+  }).mount('body')
